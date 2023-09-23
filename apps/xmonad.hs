5c74e200eed6456cbd6f741217e143a9813d7375
Lucien Iseli <lucien.iseli93@gmail.com>
2023-09-23 10:17:16 +0200

    Update xmonad config with xmobar + ewmh

--- apps/xmonad.hs
+++ apps/xmonad.hs
@@ -5,6 +5,11 @@ import XMonad.Util.NamedActions (NamedAction)
 import Data.Map (Map)
 import XMonad.Util.SpawnOnce (spawnOnce)
 import Graphics.X11.ExtraTypes.XF86
+import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
+import XMonad.Hooks.DynamicLog (xmobarProp)
+import System.Directory.Internal.Prelude (getEnv)
+import System.Environment (getEnvironment)
+import GHC.IO (unsafePerformIO)
 
 -- Increase brightness: xbacklight -inc 5
 -- Decrease brightness: xbacklight -dec 5
@@ -15,9 +20,6 @@ import Graphics.X11.ExtraTypes.XF86
 -- Toggle mute sound: amixer set Master toggle
 
 -- Battery: cat /sys/class/power_supply/BAT0/capacity
-
-myTerminal :: String
-myTerminal = "wezterm"
 
 -- TODO: Use xmonad's way to set wallpaper?
 setWallpaper :: String
@@ -32,6 +34,7 @@ myKeys0 :: [((KeyMask, KeySym), X ())]
 myKeys0 =
   [ ((mod1Mask, xK_p), spawn "rofi -show drun")
   , ((mod1Mask, xK_w), spawn setWallpaper)
+  , ((mod1Mask, xK_Return), spawn "wezterm") -- TODO: myTerminal
   , ((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
   , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 5%+") , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 5%-")
   , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 5")
@@ -40,10 +43,11 @@ myKeys0 =
 
 myStartupHook :: X ()
 myStartupHook = do
-  spawn "xsetroot -cursor_name left_ptr" -- For some reason, this is needed for XMonad to use the defined default cursor theme. Otherwise, XMonad still uses the X cursor
-  spawn setWallpaper
+  -- Otherwise, xmonad still uses X cursor by default
+  spawn "xsetroot -cursor_name left_ptr"
+  spawnOnce setWallpaper
 
-main = xmonad $ def
+myConfig myTerminal = def
   { terminal = myTerminal
   --, keys = myKeys TODO: Use myKeys and not additionalKeys
   , startupHook = myStartupHook
@@ -51,3 +55,10 @@ main = xmonad $ def
   `additionalKeys`
   myKeys0
 
+main :: IO()
+main = do
+  -- Get terminal from environment
+  myTerminal <- getEnv "TERMINAL"
+  -- Make xmonad EWMH compliant with proper fullscreen (Extended Window Manager Hints)
+  -- https://specifications.freedesktop.org/wm-spec/wm-spec-1.3.html
+  xmonad . ewmhFullscreen . ewmh . xmobarProp $ myConfig myTerminal
